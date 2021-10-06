e1c2c87a13e1b78f3119f7ec23bac51e1bafbf67
Sylvio Maggessi <sylviodev@gmail.com>
2021-10-05 22:04:49 -0300

    initial commit

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+import { IUserRepository } from '../../repositories/IUsersRepository';
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+  ) {}
+  async execute({
+    name,
+    username,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
