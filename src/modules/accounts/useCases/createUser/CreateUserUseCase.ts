9bda31a20bdd7c4c05ae44794e75aab2d92f1016
Sylvio Maggessi <sylviodev@gmail.com>
2021-10-05 22:13:26 -0300

    removing username column of users table

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,7 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
-import { UsersRepository } from '../../repositories/implementations/UsersRepository';
 import { IUserRepository } from '../../repositories/IUsersRepository';
 
 @injectable()
@@ -12,14 +11,12 @@ class CreateUserUseCase {
   ) {}
   async execute({
     name,
-    username,
     email,
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
     await this.usersRepository.create({
       name,
-      username,
       email,
       password,
       driver_license,
